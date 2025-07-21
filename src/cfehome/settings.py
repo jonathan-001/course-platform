9acaa1ce558d344da4c0d19565d49e6d28c89a5b
Anshuman Singh <anshumansingh9807@gmail.com>
2025-07-22 01:31:26 +0530

    static setting added

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+LOCAL_CDN = BASE_DIR.parent / "local-cdn"
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -115,7 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+
+#production for static -- whitenoise   nginx
 STATIC_URL = 'static/'
+
+#  nginx
+MEDIA_URL = 'media/'
+MEDIA_ROOT = LOCAL_CDN / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
